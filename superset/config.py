b788eb80ffd1f10902af81138447c25fed2dfc06
Cailei Lu <lucailei2014@xiaochuankeji.cn>
2020-05-22 15:16:07 +0800

    MOD: pip update

--- superset/config.py
+++ superset/config.py
@@ -806,7 +806,7 @@ SEND_FILE_MAX_AGE_DEFAULT = 60 * 60 * 24 * 365  # Cache static resources
 # SQLALCHEMY_DATABASE_URI by default if set to `None`
 SQLALCHEMY_EXAMPLES_URI = None
 
-SQLALCHEMY_POOL_SIZE = 20
+SQLALCHEMY_POOL_SIZE = 50
 SQLALCHEMY_POOL_TIMEOUT = 300
 
 # Some sqlalchemy connection strings can open Superset to security risks.
